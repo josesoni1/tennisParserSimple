ce9175a6543bbc614b151dca6fae178958e12ed1
josesoni1 <josesoni1@gmail.com>
2018-12-09 10:52:07 -0600

    Update src/tennisparsersimple/TennisParserSimple.java
    remove unused imports and correct identation

--- src/tennisparsersimple/TennisParserSimple.java
+++ src/tennisparsersimple/TennisParserSimple.java
@@ -9,21 +9,15 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.LocalDate;
-import java.time.Month;
-import java.time.Period;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalUnit;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Stream;
 
 
 /**
@@ -44,7 +38,7 @@ public class TennisParserSimple {
         println(sta +" "+fin);
         String direc;
         URL url;
-        List< BufferedReader> lista = new ArrayList<BufferedReader>();
+        List< BufferedReader> lista = new ArrayList<>();
         while(sta.isBefore(fin)){
             System.out.println(sta);
             direc = "http://www.tennislive.net/atp-men/"+sta+"/";
@@ -59,11 +53,9 @@ public class TennisParserSimple {
         } catch (MalformedURLException ex) {
             Logger.getLogger(LocalDate.class.getName()).log(Level.SEVERE, null, ex);
             println("pelation");
-            continue;
         }   catch (IOException ex) {
                 Logger.getLogger(TennisParserSimple.class.getName()).log(Level.SEVERE, null, ex);
                 println("pelation");
-                continue;
             }
         }
         
@@ -116,38 +108,38 @@ public class TennisParserSimple {
                 //println(aName);
                 //println(bName);
                 if(aPar[2].length()>(aPar[2].indexOf("\">")+2))
-                aSets=aPar[2].charAt(aPar[2].indexOf("\">")+2)+"";
+                    aSets=aPar[2].charAt(aPar[2].indexOf("\">")+2)+"";
                 if(bPar[1].length()>bPar[1].indexOf("\">")+2)
-                bSets=bPar[1].charAt(bPar[1].indexOf("\">")+2)+"";
+                    bSets=bPar[1].charAt(bPar[1].indexOf("\">")+2)+"";
                 //println(aSets);
                 //println(bSets);
                 if(aPar[3].length()>(aPar[3].indexOf("\">")+2))
-                aS1=aPar[3].charAt(aPar[3].indexOf("\">")+2)+"";
+                    aS1=aPar[3].charAt(aPar[3].indexOf("\">")+2)+"";
                 if(bPar[2].length()>bPar[2].indexOf("\">")+2)
-                bS1=bPar[2].charAt(bPar[2].indexOf("\">")+2)+"";
+                    bS1=bPar[2].charAt(bPar[2].indexOf("\">")+2)+"";
                 //println(aS1);
                 //println(bS1);
                 if(aPar[4].length()>(aPar[4].indexOf("\">")+2))
-                aS2=aPar[4].charAt(aPar[4].indexOf("\">")+2)+"";
+                    aS2=aPar[4].charAt(aPar[4].indexOf("\">")+2)+"";
                 if(bPar[3].length()>bPar[3].indexOf("\">")+2)
-                bS2=bPar[3].charAt(bPar[3].indexOf("\">")+2)+"";
+                    bS2=bPar[3].charAt(bPar[3].indexOf("\">")+2)+"";
                 //println(aS2);
                 //println(bS2);
                 //println("TESTS");
                 //println(aPar[5].length() +" ; "+ aPar[5].indexOf("\">")+2);
                 //println(aPar[5].length()>(aPar[5].indexOf("\">")+2));
                 if(aPar[5].length()>(aPar[5].indexOf("\">")+2))
-                aS3=aPar[5].charAt(aPar[5].indexOf("\">")+2)+"";
+                    aS3=aPar[5].charAt(aPar[5].indexOf("\">")+2)+"";
                 if(bPar[4].length()>bPar[4].indexOf("\">")+2)
-                bS3=bPar[4].charAt(bPar[4].indexOf("\">")+2)+"";
+                    bS3=bPar[4].charAt(bPar[4].indexOf("\">")+2)+"";
                 if(aPar[6].length()>aPar[6].indexOf("\">")+2)
-                aS4=aPar[6].charAt(aPar[6].indexOf("\">")+2)+"";
+                    aS4=aPar[6].charAt(aPar[6].indexOf("\">")+2)+"";
                 if(bPar[5].length()>bPar[5].indexOf("\">")+2)
-                bS4=bPar[5].charAt(bPar[5].indexOf("\">")+2)+"";
+                    bS4=bPar[5].charAt(bPar[5].indexOf("\">")+2)+"";
                 if(aPar[7].length()>aPar[7].indexOf("\">")+2)
-                aS5=aPar[7].charAt(aPar[7].indexOf("\">")+2)+"";
+                    aS5=aPar[7].charAt(aPar[7].indexOf("\">")+2)+"";
                 if(bPar[6].length()>bPar[6].indexOf("\">")+2)
-                bS5=bPar[6].charAt(bPar[6].indexOf("\">")+2)+"";
+                    bS5=bPar[6].charAt(bPar[6].indexOf("\">")+2)+"";
                 int y,m,d;
                 //println(date + " | "+date.substring(0,2)+ " | "+date.substring(3,5));
                 //println(date);
